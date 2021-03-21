bc70cc9bc80f7b98d4adb1397283452d2c082887
lino4000 <lino4000@protonmail.com>
2021-03-21 20:43:39 -0300

    Start

---
+++ src/main/java/com/lino4000/petophile/PetophileApplication.java
@@ -0,0 +1,13 @@
+package com.lino4000.petophile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetophileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetophileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lino4000/petophile/PetophileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lino4000.petophile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetophileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
